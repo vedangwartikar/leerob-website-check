8b75031a8b6b6d15602b0e1e8b3500cc449cf606
michaelangrivera <mar@michaelangrivera.com>
2023-09-07 09:40:17 -0400

    update static images

--- app/components/Photos.tsx
+++ app/components/Photos.tsx
@@ -1,15 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 
-import brighton from 'public/images/main-page/brighton.jpg'
-import closeup from 'public/images/main-page/closeup.jpg'
-import colorado from 'public/images/main-page/colorado.jpg'
-import doggos from 'public/images/main-page/doggos.jpg'
-import girlfrield from 'public/images/main-page/girlfriend.jpg'
-import hawaii from 'public/images/main-page/hawaii.jpg'
-import hawaiii2 from 'public/images/main-page/hawaiii2.jpg'
-import honolulu from 'public/images/main-page/honolulu.jpg'
-import leadville from 'public/images/main-page/leadville.jpg'
-import malibu from 'public/images/main-page/malibu.jpg'
 import seattle from 'public/images/main-page/seattle.jpg'
 import setup from 'public/images/main-page/setup.jpg'
 import { HoverPop } from './Animations'
@@ -17,18 +8,16 @@ import { Carousel } from './Carousel'
 
 export function Photos() {
   return (
-    <div className="flex-1 columns-2 sm:columns-3 gap-3 my-8">
+    <div className="flex-1 columns-2 sm:columns-3 gap-2 my-8">
       <HoverPop>
         <div className="relative h-40 mb-4 rotate-1 my-1">
           <Carousel
             images={[
-              <Image
+              <img
                 key={'hawaii'}
                 alt="somehwere in hawaii"
-                src={hawaii}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/hawaii.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover"
               />,
               <Image
@@ -48,14 +37,12 @@ export function Photos() {
         <div className="relative h-40 md:h-80 mb-4 sm:mb-0 -rotate-1  ">
           <Carousel
             images={[
-              <Image
+              <img
                 placeholder="blur"
                 key={'hawaiii2'}
                 alt="somehwere in hawaii"
-                src={hawaiii2}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/hawaiii2.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover object-[-16px] sm:object-center"
               />,
               <Image
@@ -76,24 +63,19 @@ export function Photos() {
         <div className="relative h-80 lg:h-80 sm:mb-4 -rotate-1 my-1">
           <Carousel
             images={[
-              <Image
-                placeholder="blur"
+              <img
                 key={'colorado'}
                 alt="Me in some colorado mountains during the summer"
-                src={colorado}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/colorado.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover object-top sm:object-center"
               />,
               <Image
-                placeholder="blur"
                 key={'closeup'}
                 alt="somewhere in utah skiing"
-                src={closeup}
-                fill
-                sizes="(max-width: 768px) 213px, 33vw"
-                priority
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/closeup.jpg'}
+                width={213}
+                height={340}
                 className="rounded-lg object-cover"
               />,
             ]}
@@ -101,28 +83,25 @@ export function Photos() {
         </div>
       </HoverPop>
       <HoverPop>
-        <div className="relative h-40 mb-4 sm:mb-0 rotate-1 my-2">
+        <div className="relative h-40 mb-4 sm:mb-0 my-2">
           <Carousel
             images={[
-              <Image
+              <img
                 placeholder="blur"
                 key={'brighton'}
                 alt="friend and I at Brighton, UT"
-                src={brighton}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/brighton.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
-                className="rounded-lg object-cover"
+                className="rounded-lg   object-scale-down mt-1"
               />,
-              <Image
+              <img
+                className="w-full h-[200px] rounded-full  object-scale-down "
                 placeholder="blur"
                 key={'girlfriend'}
                 alt="my girlfriend and I "
-                src={girlfrield}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/girlfriend.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
-                className="rounded-lg object-cover"
+                height={50}
               />,
             ]}
           />
@@ -132,24 +111,20 @@ export function Photos() {
         <div className="relative h-40 mb-4 rotate-1 my-2">
           <Carousel
             images={[
-              <Image
+              <img
                 placeholder="blur"
                 key={'malibu'}
                 alt="cycling in Malibu, CA"
-                src={malibu}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/malibu.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover"
               />,
-              <Image
+              <img
                 placeholder="blur"
                 key={'doggos'}
                 alt="my dogs!"
-                src={doggos}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/doggos.jpg'}
                 sizes="(max-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover"
               />,
             ]}
@@ -160,24 +135,20 @@ export function Photos() {
         <div className="relative h-80 -rotate-1 my-1">
           <Carousel
             images={[
-              <Image
+              <img
                 placeholder="blur"
                 key={'honolulu'}
                 alt="friends and I completing the honolulu marathon"
-                src={honolulu}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/honolulu.jpg'}
                 sizes="(min-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover"
               />,
-              <Image
+              <img
                 placeholder="blur"
                 key={'leadville'}
                 alt="me at the leadville marathon"
-                src={leadville}
-                fill
+                src={'https://media.michaelangrivera.com/michaelangeloio/main-page/leadville.jpg'}
                 sizes="(min-width: 768px) 213px, 33vw"
-                priority
                 className="rounded-lg object-cover"
               />,
             ]}
